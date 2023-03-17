fbb254538703e4eed67bd2c4aaa8e4a65adfbdb0
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-03-17 23:54:25 +0530

    sum of n integers using recursive function.

---
+++ Recurssive Function- sum of n integers using recursive function..py
@@ -0,0 +1,12 @@
+#Recurssive Function:
+#def-Function which calls itself repetedly till stopping condition occurs.
+
+#sum of n integers using recursive function.
+
+def sum(n):
+    if n==1:
+        return 1
+    else:
+        return n+sum(n-1)
+r=sum(6)
+print('result=',r)
